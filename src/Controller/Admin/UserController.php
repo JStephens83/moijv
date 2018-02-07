5e381254c43f19d71faa0dd62503bf6be4f4a470
J.Stephens <jeremystephens2@gmail.com>
2018-02-07 11:43:12 +0100

    Added links between user and products, gave name to the detail's method route

--- src/Controller/Admin/UserController.php
+++ src/Controller/Admin/UserController.php
@@ -3,6 +3,7 @@
 //Ce fichier a été crée dans Controller (avec php bin/console make:controller UserController dans la cmd) et on l'a bougé dans Admin, on a donc changé le chemin ci-dessous
 namespace App\Controller\Admin;
 
+use App\Entity\User;
 use App\Repository\UserRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -15,23 +16,37 @@ class UserController extends Controller
      */
     // On modifie le nom de la méthode pour refléter ce que l'on veut faire (ici, récupérer la liste des users)
     public function getList(UserRepository $userRepo)// vaut: (App\Repository\UserRepository $userRepo) -> cf. cours.txt sur les injections de dépendances
+//user repository = lien entre BDD et le controller
     {
         // Notre repository est injecté en paramètres de l'action (la méthode) de notre controller. $userRepo contient donc une instance (un objet) prète à l'emploi de Class UserRepository.
-        //Cet objet nous sert à récupérer notre liste de users.
+        //Cet objet nous sert à récupérer notre liste de users, à les stocker puis à les afficher et à les injecter dans notre vue list_user.html.twig.
         $users = $userRepo->findAll();
         return $this->render('admin/list_user.html.twig',[
                 'users' => $users
             ]);
     }
+    //{id}: car peut varier
+    
+    
+    
     /**
-     * @Route("/admin/user/{id}")
+     * @Route("/admin/user/{id}", name="user_details")
      */
-    public function details(UserRepository $userRepo, $id) {
-        //On récupère le user 
-        $user= $userRepo->find($id);
+    public function details(User $user) {
         //on l'insère dans le template(view)
         return $this->render('admin/details_user.html.twig', [
             'user' => $user
         ]);
+        
+        //public function details(User $user) {
+        //return $this->render('admin/details_user.html.twig', [
+        //  'user' => $user
+        //]); 
+        //VAUT CI-DESSOUS:
+        //public function details(UserRepository $userRepo, $id) {
+        //$user= $userRepo->find($id);
+        //return $this->render('admin/details_user.html.twig', [
+        //   'user' => $user
+        //]);
     }
 }
